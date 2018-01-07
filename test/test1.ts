1e5f18ed25c2b6775f79cac8fd3b2acfecb6f15a
takenoko <takenoko.tech@gmail.com>
2018-01-07 14:34:56 +0900

    add .ts file

---
+++ test/test1.ts
@@ -0,0 +1,11 @@
+import * as assert from 'assert';
+
+
+describe("Test Case1", () => {
+    it("test 1", () => {
+        assert.ok(true, "this shouldn't fail")
+    })
+    it("test 2", () => {
+        assert.ok(false, "this shouldn't fail")
+    })
+})
